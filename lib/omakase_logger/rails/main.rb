ba1be4f938ec619fde4bba54d8abdc8644fad041
jiikko <n905i.1214@gmail.com>
2015-10-03 16:42:43 +0900

    a

--- lib/omakase_logger/rails/main.rb
+++ lib/omakase_logger/rails/main.rb
@@ -1,26 +1,22 @@
-module OmakaseLogger
-  module Rails
-    # unsubscribe ActionView::LogSubscriber
-    ['render_template', 'render_partial', 'render_collection'].each do |event|
-      ActiveSupport::Notifications.notifier.listeners_for("#{event}.action_view").each do |x|
-        ActiveSupport::Notifications.unsubscribe(x)
-      end
-    end
-
-    # unsubscribe xxx_fragment
-    %w(write_fragment read_fragment exist_fragment? expire_fragment expire_page write_page).each do |event|
-      ActiveSupport::Notifications.notifier.listeners_for("#{event}.action_controller").each do |x|
-        ActiveSupport::Notifications.unsubscribe(x)
-      end
-    end
-  end
-end
-
 module Rails
   module Rack
     class Logger < ActiveSupport::LogSubscriber
       def started_request_message(request)
-         'Started %s "%s" for %s at %s by %s' % [
+        # unsubscribe ActionView::LogSubscriber
+        ['render_template', 'render_partial', 'render_collection'].each do |event|
+          ActiveSupport::Notifications.notifier.listeners_for("#{event}.action_view").each do |x|
+            ActiveSupport::Notifications.unsubscribe(x)
+          end
+        end
+
+        # unsubscribe xxx_fragment
+        %w(write_fragment read_fragment exist_fragment? expire_fragment expire_page write_page).each do |event|
+          ActiveSupport::Notifications.notifier.listeners_for("#{event}.action_controller").each do |x|
+            ActiveSupport::Notifications.unsubscribe(x)
+          end
+        end
+
+        'Started %s "%s" for %s at %s by %s' % [
           request.request_method,
           request.filtered_path,
           request.ip,

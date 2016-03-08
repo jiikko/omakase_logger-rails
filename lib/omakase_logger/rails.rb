b54bc31c70906bdaf4787136c0852de3fb3630c3
jiikko <n905i.1214@gmail.com>
2016-03-08 23:50:52 +0900

    json

--- lib/omakase_logger/rails.rb
+++ lib/omakase_logger/rails.rb
@@ -1,2 +1,7 @@
 require "omakase_logger/rails/version"
 require "omakase_logger/rails/main"
+
+module OmakaseLogger
+  module Rails
+  end
+end
--- lib/omakase_logger/rails/main.rb
+++ lib/omakase_logger/rails/main.rb
@@ -1,29 +1,39 @@
-module Rails
-  module Rack
-    class Logger < ActiveSupport::LogSubscriber
-      def started_request_message(request)
-        # unsubscribe ActionView::LogSubscriber
-        ['render_template', 'render_partial', 'render_collection'].each do |event|
-          ActiveSupport::Notifications.notifier.listeners_for("#{event}.action_view").each do |x|
-            ActiveSupport::Notifications.unsubscribe(x)
-          end
-        end
+require 'json'
+require 'logger'
 
-        # unsubscribe xxx_fragment
-        %w(write_fragment read_fragment exist_fragment? expire_fragment expire_page write_page).each do |event|
-          ActiveSupport::Notifications.notifier.listeners_for("#{event}.action_controller").each do |x|
-            ActiveSupport::Notifications.unsubscribe(x)
-          end
-        end
+# @log = Logger.new(STDOUT); @log.formatter = OmakaseLogger::Rails::Formatter.new; @log.info "foo bar"
 
-        'Started %s "%s" for %s at %s by %s' % [
-          request.request_method,
-          request.filtered_path,
-          request.ip,
-          Time.now.to_default_s,
-          request.env['HTTP_USER_AGENT']
-        ]
-      end
+module OmakaseLogger::Rails
+  class Formatter < ::Logger::Formatter
+    def call(severity, time, program_name, message)
+      data = {
+        time: time.to_f,
+        formatted_time: time,
+        pid: $$,
+        level: severity,
+        program_name: program_name,
+        message: message
+      }
+      data.to_json + "\n"
+    rescue JSON::GeneratorError
+      (data.each_pair {|k,v| data[k] = v.scrub("?") if v.is_a?(String) }).to_json + "\n"
+    end
+  end
+end
+
+
+if defined?(ActiveSupport)
+  # unsubscribe ActionView::LogSubscriber
+  ['render_template', 'render_partial', 'render_collection'].each do |event|
+    ActiveSupport::Notifications.notifier.listeners_for("#{event}.action_view").each do |x|
+      ActiveSupport::Notifications.unsubscribe(x)
+    end
+  end
+
+  # unsubscribe xxx_fragment
+  %w(write_fragment read_fragment exist_fragment? expire_fragment expire_page write_page).each do |event|
+    ActiveSupport::Notifications.notifier.listeners_for("#{event}.action_controller").each do |x|
+      ActiveSupport::Notifications.unsubscribe(x)
     end
   end
 end
